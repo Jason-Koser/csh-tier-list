d6de034ddce7de7253cd23be62c8f64f12d4a984
jck1098 <johndoe@example.com>
2023-04-08 22:36:16 -0400

    Started board

---
+++ src/Components/Board.js
@@ -0,0 +1,26 @@
+function Board() {
+  return (
+    <div className="Board">
+      <div className="Row">
+        <div className="Label">S</div>
+      </div>
+      <div className="Row">
+        <div className="Label">A</div>
+      </div>
+      <div className="Row">
+        <div className="Label">B</div>
+      </div>
+      <div className="Row">
+        <div className="Label">C</div>
+      </div>
+      <div className="Row">
+        <div className="Label">D</div>
+      </div>
+      <div className="Row">
+        <div className="Label">F</div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
