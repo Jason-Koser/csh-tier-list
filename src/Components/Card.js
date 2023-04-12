23e82dcf14c03b9afc303370753417a4acc940a9
jck1098 <johndoe@example.com>
2023-04-11 22:39:19 -0400

    Got Drag and drop to work with issues

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,14 @@
+
+
 const Card = (props) => {
+  function handleDragStart(event) {
+    event.dataTransfer.setData("Card Key", props.key);
+    console.log("Drag Start");
+  }
+
   return (
-    <div className="Card" draggable="true">
-      <img src={props.imagePath} alt={props.description} width={100}/>
+    <div className="Card" draggable="true" onDragStart={handleDragStart}>
+      <img src={props.imagePath} alt={props.description} width={100} />
     </div>
   );
 };
--- src/Components/Tier.js
+++ src/Components/Tier.js
@@ -1,11 +1,23 @@
+import React from "react";
 import { useState } from "react";
 import Card from "./Card";
 
 function Tier(props) {
   const [cards, setCards] = useState([]);
 
+  function handleDragOver(event) {
+    event.preventDefault();
+    console.log(`Over tier ${props.tierName}`);
+  }
+
+  function handleOnDrop(event) {
+    const cardTransfered = event.dataTransfer.getData("Card Key");
+    setCards(cards.concat(cardTransfered));
+    console.log("Card", cardTransfered);
+  }
+
   return (
-    <div className="Tier">
+    <div className="Tier" onDrop={handleOnDrop} onDragOver={handleDragOver}>
       <div className="Label" style={{ backgroundColor: props.color }}>
         {props.tierName}
       </div>
